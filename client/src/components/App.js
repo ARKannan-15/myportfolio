13c8a9d5ee54c0b8259db257ed8d2148c1e597f2
Kannan A.R <jaguar.kannan24@gmail.com>
2023-02-05 19:14:46 +0530

    routes of mai

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -16,7 +16,7 @@ function App()
     <HashRouter>
    
       <Routes>
-      <Route path="/" element={<Home />} />  
+      <Route exact path="/" element={<Home />} />  
       <Route path="/about" element={<Aboutme />} />   
       <Route path="/work" element={<Work />} />     
       <Route path="/contact" element={<Touch />}/>
