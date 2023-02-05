22f3951a7ee25eee95582ee469acfdbd3183e968
Kannan A.R <jaguar.kannan24@gmail.com>
2023-02-05 19:55:22 +0530

    routing8

---
+++ client/src/components/Clubs.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Header from "./Header";
+import Club from './Club';
+import Contact from "./Contact";
+
+function Clubs()
+{
+    <div>
+        <Header />
+        <Club />
+        <Contact />
+    </div>
+}
+
+export default Clubs;
